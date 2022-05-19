6460b442ba94d248bcf83240256cb93eee9e34b1
Kwinta <94843940+KwintaJ@users.noreply.github.com>
2022-05-19 15:34:18 +0200

    Add files via upload

---
+++ Templates/program.c
@@ -0,0 +1,23 @@
+//############################################//
+//                                            //
+//    Jan Kwinta                DD.MM.YYYY    //
+//                                            //
+//                                            //
+//    Projekt: ... Zadanie ...                //
+//    Plik: program.c                         //
+//                                            //
+//    Program ...                             //
+//                                            //
+//############################################//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
